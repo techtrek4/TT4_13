0c7284c3f7ce94ab775243eea9c0e7ccad9047d4
lohkaiyi <51329075+lohkaiyi@users.noreply.github.com>
2022-03-25 12:27:44 +0800

    import useState

--- frontend/src/components/pages/LoginPage.js
+++ frontend/src/components/pages/LoginPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import { Link } from "react-router-dom";
 
 import "../../App.css";
@@ -16,7 +16,7 @@ export default function LoginPage() {
   const handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
-    setDetails({ ...loginDetails, [name]: value });
+    setLoginDetails({ ...loginDetails, [name]: value });
   };
 
   return (
