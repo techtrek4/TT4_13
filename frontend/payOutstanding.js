9d29bf672253a05982714de9403ede697b0bad74
Vanessa Au <vanessa.auxueqi@gmail.com>
2022-03-25 14:52:08 +0800

    payOutstanding update

---
+++ frontend/payOutstanding.js
@@ -0,0 +1,44 @@
+Vue.createApp({
+    data() {
+        return {
+            username: '',
+            all_loans: ['mobile loan'],
+            selected_loan: '',
+            amount_to_pay: ''
+        }
+    },
+
+    created() {
+        console.log("============== payOutstanding ================")
+            
+        let url = ""
+        axios.get(url)
+        .then(response => {
+            this.all_loans = response.data.records
+        })
+        .catch(error => { 
+            console.log(error.message)
+        })  
+    },
+
+    methods: {
+        getLoanAmount() {
+            console.log("============== getLoanAmount ================")
+            
+            let url = ""
+            
+            axios.get(url, {
+                params: {
+                    booking_id : loan_id
+                }
+            })
+            .then(response => {
+                this.all_loans = response.data.records
+            })
+            .catch(error => { 
+                console.log(error.message)
+            })  
+        }
+    }
+
+}).mount("#main-box")
