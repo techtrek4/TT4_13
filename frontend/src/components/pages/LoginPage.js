490a5f944bdd2f85e72b28a1e9d25b3d1f21cfb1
bijiaen <77262000+bijiaen@users.noreply.github.com>
2022-03-25 17:03:16 +0800

    authenticate

--- frontend/src/components/pages/LoginPage.js
+++ frontend/src/components/pages/LoginPage.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import axios from "axios";
 import { useQuery } from "react-query";
 
@@ -10,9 +11,14 @@ export default function LoginPage() {
     username: "",
     password: "",
   });
+  // const history = useHistory();
+
+  const [result, setResult] = useState();
 
   const handleSubmit = (e) => {
     console.log("form submitted");
+    e.preventDefault();
+    loginAuth(loginDetails.username, loginDetails.password);
   };
 
   const handleChange = (e) => {
@@ -22,21 +28,43 @@ export default function LoginPage() {
   };
 
   const loginAuth = (username, password) =>
-    axios.get("/login-auth", {
-      params: {
-        id: username,
-        pw: password,
-      },
-    });
+    axios
+      .get("http://127.0.0.1:5000/login_auth", {
+        params: {
+          username: username,
+          password: password,
+        },
+      })
+      .then((res) => {
+        setResult(res);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
 
   // const authenticate = useQuery(["authenticate", username], () =>
   //   loginAuth(username, password)
   // );
+  console.log(result);
+
+  useEffect(() => {
+    // console.log(result.response.data.message);
+    if (result) {
+      if (result.data) {
+        if (result.data.message) {
+          console.log(result.data.message);
+          if ((result.data.message = "success")) {
+            // history.push("/home");
+          }
+        }
+      }
+    }
+  }, [result]);
 
   return (
     <div className="text-center m-5-auto">
       <h2>Login</h2>
-      <form action="/home" onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <p>
           <label>Username</label>
           <br />
