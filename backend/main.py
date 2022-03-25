38a3ff86bf50e7935f06aca274feb6a96ceff975
Jaivignesh Venugopal <smugs1995@gmail.com>
2022-03-25 11:18:54 +0800

    set up backend template

---
+++ backend/main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+
+# main driver function
+if __name__ == '__main__':
+
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run()
